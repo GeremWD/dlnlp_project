4040f85c1b1f04a1589f1e8214c3c816a552c602
gerem <gerem.hutin@gmail.com>
2020-03-01 21:40:51 +0100

    _

--- train.py
+++ train.py
@@ -7,6 +7,9 @@ import numpy as np
 def train_model(model, epochs, train_data, train_masks, train_targets, dev_data, dev_masks, dev_targets, lr, batch_size):
     max_len = train_data.shape[1]
     embedding_dim = train_data.shape[2]
+    dev_data = torch.Tensor(dev_data).float()
+    dev_masks = torch.Tensor(dev_masks)
+    dev_targets = torch.LongTensor(dev_targets)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss = nn.CrossEntropyLoss()
@@ -20,25 +23,18 @@ def train_model(model, epochs, train_data, train_masks, train_targets, dev_data,
         # Shuffle index
         p = np.random.permutation(n_examples)
         # Get all batches in array form
-        
         batches_x = torch.from_numpy(train_data[:n_examples][p].reshape(-1, batch_size, max_len, embedding_dim)).float()
         batches_mask = torch.from_numpy(train_masks[:n_examples][p].reshape(-1, batch_size, max_len))
         batches_y = torch.from_numpy(train_targets[:n_examples][p].reshape(-1, batch_size, max_len))
         
         train_loss = 0
-        i = 0
         for batch_x, batch_mask, batch_y in zip(batches_x, batches_mask, batches_y):
-            i += 1
-            print(i / len(batches_x))
             # zero the parameter gradients
             optimizer.zero_grad()
 
             #prediction and loss
             pred = model(batch_x, batch_mask)
             loss_val = loss(pred.view((-1, 5)), batch_y.view((-1,)))
-            #print(loss_val)
-            #loss_val = loss_val[:, :]
-
             train_loss += loss_val
 
             #backward and updates
@@ -46,10 +42,13 @@ def train_model(model, epochs, train_data, train_masks, train_targets, dev_data,
             torch.nn.utils.clip_grad_value_(model.parameters(), 5.)
             optimizer.step()
 
-        dev_pred = model(torch.Tensor(dev_data).float(), torch.Tensor(dev_masks))
-        dev_loss = loss(dev_pred.view((-1, 5)), torch.LongTensor(dev_targets).view((-1,))).item()
+        dev_pred = model(dev_data, dev_masks)
+        dev_loss = loss(dev_pred.view((-1, 5)), dev_targets.view(-1,)).item()
+        _, predicted = torch.max(dev_pred, 2)
+        total = dev_targets.view((-1,)).size(0)
+        dev_accuracy = (predicted == dev_targets).sum().item() / total
         train_loss =  train_loss.item() / len(train_data)
-        print("train_loss : ", train_loss, "dev_loss : ", dev_loss)
+        print("Epoch", i, " train_loss :", train_loss, " dev_loss :", dev_loss, " dev_accuracy :", dev_accuracy)
 
 
 if __name__=='__main__':
@@ -61,9 +60,4 @@ if __name__=='__main__':
     dev_targets = np.load("data/testa_targets.npy")
     embedding_dim = train_data.shape[2]
     model = TransformerEncoder(2, embedding_dim, 8, 2*embedding_dim, 0.15, 5).float()
-
-    #test_pred = model(torch.Tensor(train_data[:1]).float(), torch.Tensor(dev_masks[:1]))
-    #print(dev_masks[0])
-    #print(test_pred)
-
     train_model(model, 10, train_data, train_masks, train_targets, dev_data, dev_masks, dev_targets, 0.001, 16)
