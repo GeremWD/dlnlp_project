358f64482e8da2c67fd99409f26dd7b6b5a747be
gerem <gerem.hutin@gmail.com>
2020-03-02 20:39:23 +0100

    _

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from transformer import TransformerEncoder
 import torch
 from torch import nn
 import numpy as np
+from sklearn.metrics import f1_score
 
 
 def train_model(model, epochs, train_data, train_masks, train_targets, dev_data, dev_masks, dev_targets, lr, batch_size):
@@ -28,7 +29,9 @@ def train_model(model, epochs, train_data, train_masks, train_targets, dev_data,
         batches_y = torch.from_numpy(train_targets[:n_examples][p].reshape(-1, batch_size, max_len))
         
         train_loss = 0
+        j = 0
         for batch_x, batch_mask, batch_y in zip(batches_x, batches_mask, batches_y):
+            j += 1
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -41,14 +44,15 @@ def train_model(model, epochs, train_data, train_masks, train_targets, dev_data,
             loss_val.backward()
             torch.nn.utils.clip_grad_value_(model.parameters(), 5.)
             optimizer.step()
-
-        dev_pred = model(dev_data, dev_masks)
-        dev_loss = loss(dev_pred.view((-1, 5)), dev_targets.view(-1,)).item()
-        _, predicted = torch.max(dev_pred, 2)
-        total = dev_targets.view((-1,)).size(0)
-        dev_accuracy = (predicted == dev_targets).sum().item() / total
-        train_loss =  train_loss.item() / len(train_data)
-        print("Epoch", i, " train_loss :", train_loss, " dev_loss :", dev_loss, " dev_accuracy :", dev_accuracy)
+            if j % 100 == 0:
+                dev_pred = model(dev_data, dev_masks)
+                dev_loss = loss(dev_pred.view((-1, 5)), dev_targets.view(-1,)).item()
+                _, predicted = torch.max(dev_pred, 2)
+                total = dev_targets.view((-1,)).size(0)
+                dev_accuracy = (predicted == dev_targets).sum().item() / total
+                dev_f1 = f1_score(dev_targets.view((-1,)), predicted.view((-1,)), average='weighted')
+                train_loss =  train_loss.item() / len(train_data)
+                print("Epoch", i, " train_loss :", train_loss, " dev_loss :", dev_loss, " dev_accuracy :", dev_accuracy, " dev_f1 :", dev_f1)
 
 
 if __name__=='__main__':
